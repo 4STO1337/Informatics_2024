9e45a153883bb8a39dc0002273670fbefde1baa7
4STO1337 <hdshbdbshxbxjxnx@gmail.ru>
2024-11-05 01:19:50 +0300

    LAB4

--- golang/main.go
+++ golang/main.go
@@ -5,3 +5,31 @@ import "fmt"
 func main() {
 	fmt.Println("Samvel_Melikyan_Artemovich")
 }
+
+package main
+
+import "fmt"
+import (
+ "fmt"
+
+ "isuct.ru/informatics2022/laba4ab"
+)
+
+func main() {
+ fmt.Println("Samvel_Melikyan_Artemovich")
+  laba4ab.Readylaba4ab()
+}
+
+package main
+
+import "fmt"
+import (
+ "fmt"
+
+ "isuct.ru/informatics2022/laba4ab"
+)
+
+func main() {
+ fmt.Println("Melikyan Samvel")
+  laba4ab.Readylaba4ab()
+}
